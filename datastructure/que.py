dd344fc8cd3f3bc87a6be13ea06ec27f352fb584
HJ <de0978@gmail.com>
2021-11-21 22:57:49 +0900

    add que.py

---
+++ datastructure/que.py
@@ -0,0 +1,15 @@
+# 큐 Queue
+# 연결리스트로 큐 구현시 삽입/삭제 : O(1)
+# deque == Double-Ended QUEue
+
+from queue import Queue # thread-safe 기능이 포함되어 있어 실제 속도가 느리다
+from collections import deque # thread-safe 하지 않기 때문에 속도가 빨라 알고리즘 풀이에 적합한 큐
+dq= deque()
+dq.append(123)
+dq.append(456)
+dq.append(789)
+
+print(f"size : {len(dq)}")
+
+while len(dq) > 0:
+    print(dq.popleft())

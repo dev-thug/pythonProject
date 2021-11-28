286285d27ed470b5c64d8c19dd07ea3d6363a3a3
HJ <de0978@gmail.com>
2021-11-28 16:57:44 +0900

    add boj.kr/1149 문제해석오류

---
+++ problem/1149.py
@@ -0,0 +1,39 @@
+n = int(input())
+
+result = 0
+rgb = []
+
+for i in range(n):
+    costs = list(map(int, input().split(" ")))
+    if i == 0:
+        cost = min(costs)
+        rgb.append(costs.index(cost))
+
+# for i in range(n):
+#     costs = list(map(int, input().split(" ")))
+#     if i == 0:
+#
+#         cost = min(costs)
+#         rgb[i] = costs.index(cost)
+#         result += cost
+#     elif i > 2:
+#
+#         temp = costs.copy()
+#         dx = temp[rgb[i - 1]]
+#         dy = temp[rgb[i - 2]]
+#         temp.remove(dx)
+#         temp.remove(dy)
+#         cost = temp.pop()
+#         print(cost)
+#         rgb[i] = costs.index(cost)
+#         result += cost
+#     else:
+#
+#         temp = costs.copy()
+#         temp.pop(rgb)
+#         cost = min(temp)
+#         print(cost)
+#         rgb[i] = costs.index(cost)
+#         result += cost
+
+print(result)

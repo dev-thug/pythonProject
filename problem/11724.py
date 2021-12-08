0d629c225fb55d9784dae34f8792cffeb30033d6
HJ <de0978@gmail.com>
2021-12-08 22:10:03 +0900

    update boj.kr/11724

--- problem/11724.py
+++ problem/11724.py
@@ -1,10 +1,17 @@
 N, M = list(map(int, input().split()))
 
-connected = [tuple(map(int, input().split())) for _ in range(M)]
+# (1 ≤ N ≤ 1,000, 0 ≤ M ≤ N×(N-1)/2) 이 조건 때문에 인접행렬로 그래프 구성
+connected = [[False] * N for _ in range(N)]
+visited = [False * N]
+stack = []
+for _ in range(M):
+    x, y = list(map(int, input().split()))
+    connected[x-1][y-1] = connected[y-1][x-1] = True
 
-print(connected)
+stack.append(0)
 
-# for i in g:
-#     print(i)
 
-# def dfs(now):
+
+
+
+

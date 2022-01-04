476b30f8b8b0c6a135d7be65699b367a15447bef
HJ <de0978@gmail.com>
2022-01-04 23:09:11 +0900

    add 11724 문제풀이

--- problem/11724.py
+++ problem/11724.py
@@ -1,17 +1,30 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
 N, M = list(map(int, input().split()))
 
+input = sys.stdin.readline
+
 # (1 ≤ N ≤ 1,000, 0 ≤ M ≤ N×(N-1)/2) 이 조건 때문에 인접행렬로 그래프 구성
-connected = [[False] * N for _ in range(N)]
-visited = [False * N]
-stack = []
+connected = [[False] * (N + 1) for _ in range(N + 1)]
+visited = [False] * (N + 1)
+answer = 0
+
 for _ in range(M):
     x, y = list(map(int, input().split()))
-    connected[x-1][y-1] = connected[y-1][x-1] = True
-
-stack.append(0)
+    connected[x][y] = connected[y][x] = True
 
 
+def dfs(root):
+    for i in range(1, N + 1):
+        if connected[root][i] and not visited[i]:
+            visited[i] = True
+            dfs(i)
 
 
+for i in range(1, N + 1):
+    if not visited[i]:
+        dfs(i)
+        answer += 1
 
-
+print(answer)
